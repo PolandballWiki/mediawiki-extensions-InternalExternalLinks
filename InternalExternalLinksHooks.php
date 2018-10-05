8151a6b53f6301f20d74fbb70c5e8164898d97c8
Ty <tyler@faceyspacies.com>
2018-10-05 01:21:02 -0500

    add extension

---
+++ InternalExternalLinksHooks.php
@@ -0,0 +1,14 @@
+<?php 
+class InternalExternalLinksHooks {
+	public static function onLinkerMakeExternalLink( &$url, &$text, &$link, &$attribs, $linktype ) {
+		# https://runescape.wiki/ is 23 chars 
+		#https://oldschool.runescape.wiki/ is 33 chars
+		
+		if(substr($url, 0, 23) == "https://runescape.wiki/" or 
+			substr($url, 0, 33) == "https://oldschool.runescape.wiki") {
+				$attribs["class"] = str_replace("external", "", $attribs["class"]);
+		}
+		
+		return true;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # InternalExternalLinks
-Removes the external class from external links to ourself
+
+This extension removes the external class from https://runescape.wiki/ and https://oldschool.runescape.wiki/ links. 
+
+## Installation
+```git clone https://github.com/ty-a/InternalExternalLinks``` in your extensions folder 
+
+Then add 
+```wfLoadExtension("InternalExternalLinks");```
+to your LocalSettings.php 
